3ef8b1f1630078c433f1c3b68dfc28e61765ec53
Gabriel Marinho <139128390+gabrielmarinhobsb@users.noreply.github.com>
2026-02-27 17:18:17 -0300

    Correct error: inpts is not showed in alter or add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -7,6 +7,24 @@ import { MatButtonModule } from '@angular/material/button';
   standalone: true,
   imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  isMobile: boolean = false;
+
+  ngOnInit() {
+    this.onCheckMobile();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.onCheckMobile(event);
+  }
+
+  onCheckMobile(event?: any) {
+    const width = event?.target?.innerWidth || window.innerWidth;
+    this.isMobile = width <= 768;
+    // Opcional: log para debug
+    console.log('Tela mobile?', this.isMobile, '| Largura:', width);
+  }
+}
