949d6ee34747a1a89d4ac9705c148ced3a85266d
Gabriel Marinho <biel32771@gmail.com>
2026-03-01 16:35:43 -0300

    Adjustes in layout

--- src/app/shared/components/table-financial-dialog/table-financial-dialog.component.ts
+++ src/app/shared/components/table-financial-dialog/table-financial-dialog.component.ts
@@ -13,21 +13,33 @@ import { DataTable } from '../../../models/data-table.model';
   selector: 'app-table-financial-dialog',
   standalone: true,
   imports: [CommonModule, FormsModule, MaterialModule, MatDialogModule],
-  templateUrl: './table-financial-dialog.component.html',   // 👈 faltava isso
+  templateUrl: './table-financial-dialog.component.html',
   styleUrls: ['./table-financial-dialog.component.scss'],
 })
 export class TableFinancialDialogComponent {
-
   row: DataTable = new DataTable();
+  list: DataTable[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<TableFinancialDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DataTable
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    Object.assign(this.row, data);
+    Object.assign(this.row, data.row);
+    this.list = data.list || [];
   }
 
   save() {
+    const duplicated = this.list.some(
+      (item) =>
+        item.sequencial_month === this.row.sequencial_month &&
+        item !== this.data.row
+    );
+
+    if (duplicated) {
+      alert('Já existe um registro com este mês sequencial.');
+      return;
+    }
+
     this.dialogRef.close(this.row);
   }
 
