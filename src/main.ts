468b278352423d4aca0d50946599e93d8138bbbd
Gabriel Marinho <139128390+gabrielmarinhobsb@users.noreply.github.com>
2026-05-11 17:14:00 -0300

    Config App PWA - First commit

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { appConfig } from './app/app.config';
 
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { LOCALE_ID, importProvidersFrom } from '@angular/core';
+import { LOCALE_ID, importProvidersFrom, isDevMode } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './app/shared/material/material.module';
+import { provideServiceWorker } from '@angular/service-worker';
 
 registerLocaleData(localePt);
 
@@ -15,6 +16,10 @@ bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers ?? [],
     { provide: LOCALE_ID, useValue: 'pt-BR' },
-    importProvidersFrom(BrowserAnimationsModule, MaterialModule)
-  ]
+    importProvidersFrom(BrowserAnimationsModule, MaterialModule),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 }).catch((err) => console.error(err));
