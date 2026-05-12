e3cd3404a54e45d429b0a68b885dd5070efa6838
Gabriel Marinho <biel32771@gmail.com>
2026-05-12 11:12:51 -0300

    Config App PWA - Adding button for app install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,54 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from './shared/material/material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MaterialModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isMobile: boolean = false;
+
+  deferredPrompt: any = null;
+  isInstalled = false;
 
   ngOnInit() {
     this.onCheckMobile();
+    this.initPWA();
+  }
+
+  initPWA() {
+    window.addEventListener('beforeinstallprompt', (event: any) => {
+      event.preventDefault();
+      this.deferredPrompt = event;
+    });
+
+    window.addEventListener('appinstalled', () => {
+      this.deferredPrompt = null;
+      this.isInstalled = true;
+    });
+
+    this.checkStandalone();
+  }
+
+  checkStandalone() {
+    this.isInstalled =
+      window.matchMedia('(display-mode: standalone)').matches ||
+      (window.navigator as any).standalone === true;
+  }
+
+  installPWA() {
+    if (!this.deferredPrompt) return;
+
+    this.deferredPrompt.prompt();
+
+    this.deferredPrompt.userChoice.then(() => {
+      this.deferredPrompt = null;
+    });
   }
 
   @HostListener('window:resize', ['$event'])
@@ -24,7 +59,5 @@ export class AppComponent {
   onCheckMobile(event?: any) {
     const width = event?.target?.innerWidth || window.innerWidth;
     this.isMobile = width <= 768;
-    // Opcional: log para debug
-    console.log('Tela mobile?', this.isMobile, '| Largura:', width);
   }
 }
