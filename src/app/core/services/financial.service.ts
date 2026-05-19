312390affceb6f6a47301d21d7966814048a68f3
Gabriel Marinho <biel32771@gmail.com>
2026-05-19 17:15:23 -0300

    Remove obsolete code in financial.service

--- src/app/core/services/financial.service.ts
+++ src/app/core/services/financial.service.ts
@@ -5,21 +5,6 @@ import { Table } from '../../models/table.model';
   providedIn: 'root',
 })
 export class FinancialService {
-  async getQuote(ticker: string): Promise<any | null> {
-    try {
-      const response = await fetch(
-        `https://api-financial-control-9skh.onrender.com/quote?ticker=${ticker}`,
-      );
-
-      const data = await response.json();
-
-      return data || null;
-    } catch (error) {
-      console.error('Erro ao consultar cotação', error);
-      return null;
-    }
-  }
-
   createTable(tables: any): any {
     localStorage.setItem('financial_tables', JSON.stringify(tables));
     return true;
@@ -55,28 +40,5 @@ export class FinancialService {
     sessionStorage.setItem('tables', JSON.stringify(filtered));
 
     return filtered;
-  }
-
-  async getDividends(ticker: string): Promise<any[]> {
-    try {
-      const url = `https://api-financial-control-9skh.onrender.com/dividends?ticker=${ticker}`;
-
-      const response = await fetch(url);
-      const data = await response.json();
-
-      if (!Array.isArray(data)) return [];
-
-      const dividends = data.map((d: any) => ({
-        date: new Date(d.date),
-        value: d.value,
-      }));
-
-      dividends.sort((a, b) => a.date.getTime() - b.date.getTime());
-
-      return dividends;
-    } catch (error) {
-      console.error('Erro ao buscar dividendos', error);
-      return [];
-    }
   }
 }
